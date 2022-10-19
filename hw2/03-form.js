126f7fa5e73fde4d1e0aab4bef03943caea2a966
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2022-10-19 15:53:22 -0700

    exercise 3

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,40 @@
 /** Exercise 03 - Form **/
 
 // Add your code here
+const main = () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        printOutput({
+            name: document.getElementById('nameInput').value,
+            email: document.getElementById('emailInput').value,
+            feedback: document.getElementById('textarea').value,
+            newsletter: document.querySelector('#newsletter').checked,
+        });
+
+        clearInputs();
+    });
+    form.addEventListener('reset', (event) => {
+        event.preventDefault();
+        clearInputs();
+    });
+};
+main();
+
+const clearInputs = () => {
+    document.querySelectorAll('.form-control').forEach((i) => (i.value = ''));
+    document.querySelector('#newsletter').checked = false;
+};
+
+const printOutput = (output) => {
+    console.log('========= Form Submission =========');
+    console.log(`Name: ${output.name}`);
+    console.log(`Email: ${output.email}`);
+    console.log(`Feedback: ${output.feedback}`);
+    if (output.newsletter) {
+        console.log('Newsletter: Yes, I would like to join the newsletter.');
+    } else {
+        console.log('Newsletter: No, thank you.');
+    }
+};
