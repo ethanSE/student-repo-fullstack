c5abf4be3c6661299c100d2ea1000e2775d78e04
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2022-10-25 15:02:15 -0700

    refactor getRoutes

--- hw3/getRoutes.js
+++ hw3/getRoutes.js
@@ -1,6 +1,3 @@
 module.exports = (routes) => {
-    return routes.reduce(
-        (acc = '', elem) => acc.concat(`<li><a href="/${elem}">/${elem}</a></li>`),
-        ''
-    );
+    return routes.map((elem) => `<li><a href="/${elem}">/${elem}</a></li>`).join('');
 };
